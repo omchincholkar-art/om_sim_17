3086d92e4e75dc9ee5a23d0dbc0a79575eb6fb82
omchincholkar-art <om.chincholkar@hevodata.com>
2026-04-06 09:31:43 +0530

    Update transform.sql

--- models/transform.sql
+++ models/transform.sql
@@ -1,7 +1,7 @@
 select
-  id,
-  book_name,
-  author,
-  price,
-  price * 0.9 as discounted_price
-from raw_books
+  id,
+  book_name,
+  author,
+  price,
+  price * 0.9 as discounted_price
+from raw_books;
